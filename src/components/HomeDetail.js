25b943a31ba0098de4966b30de1ebfb081d68bfd
gravityBoom <568307411@qq.com>
2017-03-30 22:53:37 +0800

    22222

--- src/components/HomeDetail.js
+++ src/components/HomeDetail.js
@@ -13,20 +13,76 @@ class HomeDetail extends React.Component{
             style_name:'',
             img:'',
             default_money:'',
-            discount:''
+            discount:'',
+			totlal:0,
+            homeDetailList:[]
 		}
 	}
-	
+
+	componentWillMount(){
+        window.fetch('/json/detail.json')
+            .then(response => {
+                return response.json();
+            })
+            .then(json=>{
+                let Json = json.data.goods_list;
+                let tempArr = [];
+
+                Json.map(item => {
+
+                    tempArr.push(
+
+						<ul className="hdgc">
+							<li className="goods">
+								<section className="detail_list">
+									<div><button></button></div>
+									<a href="javascript:void(0)">
+										<div className="div123"><img src = {item['goods_thumb']} alt=""/></div>
+										<div className="div456">
+											<p className="dgoodname">{item['goods_name']}</p>
+											<section className="goodattr">
+												<span>{item['goods_attr'].substring(0,item['goods_attr'].indexOf(' '))}</span>
+												<br />
+												<span>规格:804*545*1700</span>
+											</section>
+											<div className="fl dgood_price">￥<span>{item['shop_price']}</span><span className="fr">X1</span></div>
+										</div>
+									</a>
+								</section>
+
+								<section className="changed">
+									<button style={{ display: item['msg']=='' ?  'inline-block':'none' }} className="fr yo-btn yo-btn-changeAbtn">修改规格</button>
+									<span style={{ display: item['msg']=='' ? 'none' : 'inline-block' }}>{item['msg']}</span>
+								</section>
+							</li>
+						</ul>
+
+                    );
+
+                });
+
+                this.setState({
+                    homeDetailList:tempArr
+                });
+
+                console.log(this.state.homeDetailList)
+
+            })
+	}
+
 	componentDidMount(){
 		console.log('Mounted');
 		let homeGoodId = this.props.params['homegoodid'];
         let self = this;
-		let tempArr = [];
+
+
 		window.fetch('/api/v2/h5/index/space?is_json=1&page='+ 1 +'&space=&style=')
 		.then(response =>{
 			return response.json();
 		})
 		.then(result => {
+
+
 			let Data = result.data;
 			for(let i = 0 , len = Data.length; i < len ; i++){
                 if(Data[i]['id'] == homeGoodId){
@@ -45,70 +101,7 @@ class HomeDetail extends React.Component{
                     break;
                 }
 			}
-
-			tempArr.push(
-				<div className="hbc">
-                    <span className="goback" onClick={this.goback}></span>
-                    <Carousel loop = {true}>
-						<img className = "home_detail_banner" src={this.state.img} alt=""/>
-						<img className = "home_detail_banner" src={this.state.img} alt=""/>
-					</Carousel>
-
-					<section className="gapbar"></section>
-					<section className="art_author">
-						<div className="author_img"><img src="/home_img/author.png" alt=""/></div>
-						<div className="author_name">陈严</div>
-					</section>
-
-					<section className="gtip">
-						<header>校车儿童房</header>
-						<section className="gtype"><span className="fl">儿童房 现代</span> <span className="fr">￥17858.78/儿童房</span></section>
-						<p>学生时代的梦想，靠校车去承载，带着我们成长，可爱的上下铺汽车床，一定是小男生钟爱的大玩具！</p>
-					</section>
-
-					<section className="trans_gap">
-						儿童房商品清单
-					</section>
-
-					<ul className="hdgc">
-						<li className="goods">
-							<section className="detail_list">
-								<div><button>BBB</button></div>
-
-								<a href="javascript:void(0)">
-									<div className="div123"><img src="/home_img/author.png" alt=""/></div>
-									<div className="div456">
-										<p className="dgoodname">儿童校车双门卡通衣柜</p>
-										<section className="goodattr">
-											<span>花色/图案:黄色</span>
-											<br />
-											<span>规格:804*545*1700</span>
-										</section>
-										<div className="fl dgood_price">￥<span>1321.00</span><span className="fr">X1</span></div>
-									</div>
-								</a>
-							</section>
-
-							<section className="changed">
-								<button>修改规格</button>
-							</section>
-
-						</li>
-					</ul>
-				</div>
-			);
-
-			this.setState({
-                homeDetailList:tempArr
-			})
 		});
-
-		window.fetch('/json/detail.json')
-		.then(response => {
-			return response.json();})
-		.then(result=>{
-			//console.log(result);
-		})
 	}
 
 	goback(){
@@ -118,7 +111,38 @@ class HomeDetail extends React.Component{
 	render(){
 		return (
 			<div className = 'hd_container'>
+
+				<Scroller style={{height:'100%'}}>
+				<span className="goback" onClick={this.goback}></span>
+				<Carousel loop = {true}>
+					<img className = "home_detail_banner" src={this.state.img} alt=""/>
+					<img className = "home_detail_banner" src={this.state.img} alt=""/>
+				</Carousel>
+
+				<section className="gapbar"></section>
+				<section className="art_author">
+					<div className="author_img"><img src="/home_img/author.png" alt=""/></div>
+					<div className="author_name">陈严</div>
+				</section>
+
+				<section className="gtip">
+					<header>校车儿童房</header>
+					<section className="gtype"><span className="fl">{this.state.space_name} {this.state.style_name}</span> <span className="fr">￥{this.state.default_money}/{this.state.space_name}</span></section>
+					<p>学生时代的梦想，靠校车去承载，带着我们成长，可爱的上下铺汽车床，一定是小男生钟爱的大玩具！</p>
+				</section>
+
+				<section className="trans_gap">
+                    {this.state.space_name}商品清单
+				</section>
+
+
 				{ this.state.homeDetailList }
+
+				<section>
+					<header>购买优惠</header>
+
+				</section>
+				</Scroller>
 			</div>
 		)
 	}
